4aec90641e0fed284f4d2f44425e368ae3ddf74f
Peter Wischer <peter.wischer@lwl.org>
2023-01-07 22:57:22 +0100

    fix compiler error

--- DevCom5_Unity/Assets/Scripts/uiManager.cs
+++ DevCom5_Unity/Assets/Scripts/uiManager.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Runtime.ExceptionServices;
 using TMPro;
-using Unity.PlasticSCM.Editor.WebApi;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.UI;
